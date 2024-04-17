feeba113dde2a5cb0f88c62730af84d18c73cbf1
Woody <jo@Woodys-MacBook-Air.local>
2024-04-17 13:43:48 +0900

    connect firebase server

--- dj_app/lib/view/home.dart
+++ dj_app/lib/view/home.dart
@@ -1,10 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  Widget _buildItemWidget(doc) {
+    // final student = Student(code: doc['code'], name: doc['name'], dept: doc['dept'], phone: doc['phone']);
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        child: const Icon(Icons.delete_forever),
+      ),
+      // ************* data 이동 시킬 때 doc ***************
+      key: ValueKey(doc),
+      onDismissed: (direction) {
+        FirebaseFirestore.instance
+        .collection('students')
+        .doc(doc.id)
+        .delete();
+      },
+      // ************* data 이동 시킬 때 doc ***************
+      // child: GestureDetector(
+      //   onTap: () {
+      //     Get.to(const Update(), arguments: [
+      //       // id는 자동으로 관리되는 Property
+      //       doc.id, 
+      //       // id는 자동으로 관리되는 Property
+      //       doc['code'],
+      //       doc['name'],
+      //       doc['dept'],
+      //       doc['phone'],
+      //     ]);
+      //   },
+      child: Card(
+        child: ListTile(
+          title: Text(
+            "${doc['test']}"
+          ),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+                  .collection('test')
+                  .snapshots(), 
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }else {
+              final documents = snapshot.data!.docs;
+              return ListView(
+                children: documents.map((e) => _buildItemWidget(e)).toList(),
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
--- dj_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dj_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import file_selector_macos
+import firebase_analytics
+import firebase_core
+import firebase_database
+import firebase_storage
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
