4a6ec048b7dcc7988471c980aa8f7e758663949e
HnG8N <wondh16@gmail.com>
2024-04-21 19:33:38 +0900

    24.04.21 (일) flutter app project
    MySql과 flutter 연동 후 지도에 Marker 띄움 & Marker 클릭시 이름, 주소, 전화번호 정보 제공

---
+++ dj_app/lib/model/model_hospital_mysql.dart
@@ -0,0 +1,67 @@
+class ModelHospitalMysql {
+  // --- Field ---
+  String dutyAddr;
+  String dutyDiv;
+  String dutyDivNam;
+  String dutyEmcls;
+  String dutyEmclsName;
+  int dutyEryn;
+  String dutyInf;
+  String dutyEtc;
+  String dutyMapimg;
+  String dutyName;
+  String dutyTel1;
+  int dutyTime1c;
+  int dutyTime1s;
+  int dutyTime2c;
+  int dutyTime2s;
+  int dutyTime3c;
+  int dutyTime3s;
+  int dutyTime4c;
+  int dutyTime4s;
+  int dutyTime5c;
+  int dutyTime5s;
+  int dutyTime6c;
+  int dutyTime6s;
+  String hpid;
+  int postCdn1;
+  double postCdn2;
+  int rnum;
+  double wgs84Lat;
+  double wgs84Lon;
+
+  // Constructor
+  ModelHospitalMysql({
+    required this.dutyAddr,
+    required this.dutyDiv,
+    required this.dutyDivNam,
+    required this.dutyEmcls,
+    required this.dutyEmclsName,
+    required this.dutyEryn,
+    required this.dutyInf,
+    required this.dutyEtc,
+    required this.dutyMapimg,
+    required this.dutyName,
+    required this.dutyTel1,
+    required this.dutyTime1c,
+    required this.dutyTime1s,
+    required this.dutyTime2c,
+    required this.dutyTime2s,
+    required this.dutyTime3c,
+    required this.dutyTime3s,
+    required this.dutyTime4c,
+    required this.dutyTime4s,
+    required this.dutyTime5s,
+    required this.dutyTime5c,
+    required this.dutyTime6s,
+    required this.dutyTime6c,
+    required this.hpid,
+    required this.postCdn1,
+    required this.postCdn2,
+    required this.rnum,
+    required this.wgs84Lat,
+    required this.wgs84Lon,
+  });
+
+
+}
