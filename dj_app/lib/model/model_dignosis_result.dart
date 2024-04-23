ce4099f35a1904b382322e1ebdbb5a63359a42c1
Woody <jo@Woodys-MacBook-Air.local>
2024-04-23 13:38:05 +0900

    pull 충돌 때문에 푸쉬 해요

--- dj_app/lib/model/model_dignosis_result.dart
+++ dj_app/lib/model/model_dignosis_result.dart
@@ -8,6 +8,6 @@ class DiagnosisResult{
   });
 
   DiagnosisResult.fromMap(Map<String, dynamic> res)
-  : result = res['result'],
-    initdate = res['initdate'];
+  : result = res['result'].toString().substring(2,4),
+    initdate = res['initdate'].toString().substring(0,10);
 }
--- dj_app/lib/view/daignosis_page/diagnosis_view.dart
+++ dj_app/lib/view/daignosis_page/diagnosis_view.dart
@@ -16,6 +16,7 @@ class DiagnosisView extends StatelessWidget {
   String result = '';
   String initdate = '';
   List snapshotList = [];
+  List querySQLite = [];
   List<DiagnosisResult> data = [];
   DatabaseHandler handler = DatabaseHandler();
 
@@ -49,9 +50,10 @@ class DiagnosisView extends StatelessWidget {
       // ************* data 이동 시킬 때 doc ***************
       child: GestureDetector(
         onTap: () {
+          print(doc['result']);
           Get.to(const DiagnosisResultPage(), arguments: [
             // id는 자동으로 관리되는 Property
-            doc['email'],
+            doc['result'],
           ]);
         },
         child: SizedBox(
@@ -78,12 +80,59 @@ class DiagnosisView extends StatelessWidget {
     );
   }
 
+  // Build Chart
+  Widget _buildChart(context){
+    // Chart
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 400,
+      child: SfCartesianChart(
+        title: const ChartTitle(
+          text: '나의 당뇨병 결과 차트'
+        ),
+        legend: const Legend(
+          isVisible: true
+        ),
+        series: [
+        // ColumnSeries : 세로 막대 그래프
+        // BarSeries : 가로 막대 그래프
+        // LineSeries : 선 그래프
+        // ScatterSeries : 산점도
+        // DeveloperData의 format이 2개라서 2개를 잡아줬는데 1나 밖에 사용하지 않을 때 '_'을 사용한다.
+        LineSeries<DiagnosisResult, dynamic>(
+          color: Colors.red,
+          dataSource: data, // show data
+          xValueMapper: (DiagnosisResult chartModel, _) => chartModel.initdate.toString().substring(0,10), // x 
+          yValueMapper: (DiagnosisResult chartModel, _) => int.parse(chartModel.result), // y
+          dataLabelSettings: const DataLabelSettings(isVisible: true), // y축 값 표시
+          enableTooltip: true, // default true
+          markerSettings: const MarkerSettings(
+          isVisible: true, // 데이터 포인트 표시 여부
+          height: 5, // 데이터 포인트 높이
+          width: 5, // 데이터 포인트 너비
+        ),
+        ),
+      ],
+      // x축을 category로 표현,
+      primaryXAxis: const CategoryAxis( // 카테고리화 시키면 밑에 x축 labels들이 다 나온다.
+        title: AxisTitle(text: '날짜'), // label
+      ),
+      primaryYAxis: const NumericAxis(
+        title: AxisTitle(text: '당뇨병 확률 단위 : %'),
+        // minimum: 150000,
+      ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     userEmail = box.read('email');
     return Scaffold(
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
+
+
         child: StreamBuilder<QuerySnapshot>(
           stream: FirebaseFirestore.instance
                   .collection('dangjin')
@@ -107,57 +156,44 @@ class DiagnosisView extends StatelessWidget {
             return Column(
               children: [
                 
-                // for SQLite Search
-                FutureBuilder(
-                  future: handler.queryDiagnosis(userEmail), 
-                  builder: (context, snapshot) {
-                    
-                  },
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 400,
-                  child: SfCartesianChart(
-                    title: const ChartTitle(
-                      text: '나의 당뇨병 결과 차트'
-                    ),
-                    legend: const Legend(
-                      isVisible: true
-                    ),
-                    series: [
-                    // ColumnSeries : 세로 막대 그래프
-                    // BarSeries : 가로 막대 그래프
-                    // LineSeries : 선 그래프
-                    // ScatterSeries : 산점도
-                    // DeveloperData의 format이 2개라서 2개를 잡아줬는데 1나 밖에 사용하지 않을 때 '_'을 사용한다.
-                    LineSeries<DiagnosisResult, dynamic>(
-                      color: Colors.red,
-                      dataSource: data, // show data
-                      xValueMapper: (DiagnosisResult chartModel, _) => chartModel.initdate.toString().substring(0,10), // x 
-                      yValueMapper: (DiagnosisResult chartModel, _) => int.parse(chartModel.result), // y
-                      dataLabelSettings: const DataLabelSettings(isVisible: true), // y축 값 표시
-                      enableTooltip: true, // default true
-                      markerSettings: const MarkerSettings(
-                      isVisible: true, // 데이터 포인트 표시 여부
-                      height: 5, // 데이터 포인트 높이
-                      width: 5, // 데이터 포인트 너비
-                    ),
+                // Chart
+                _buildChart(context),
 
-                    ),
-                  ],
-                  // x축을 category로 표현,
-                  primaryXAxis: const CategoryAxis( // 카테고리화 시키면 밑에 x축 labels들이 다 나온다.
-                    title: AxisTitle(text: '날짜'), // label
-                  ),
-                  primaryYAxis: const NumericAxis(
-                    title: AxisTitle(text: '당뇨병 확률 단위 : %'),
-                    // minimum: 150000,
-                  ),
-                  ),
-                ),
+                // for SQLite Search
+                // FutureBuilder(
+                //   future: handler.queryDiagnosis(userEmail),
+                //   builder: (context, snapshot2) {
+                //     // snapshot handler.queryStudents가 갖고 있는 그 순간 순간 최신 메모리를 갖고 있다.
+                //     if (snapshot2.hasData) {
+                //       return ListView.builder(
+                //         shrinkWrap: true,
+                //         itemCount: snapshot2.data!.length,
+                //         itemBuilder: (context, index2) {
+                //           return Card(
+                //             child: Row(
+                //               children: [
+                //                 Text(
+                //                   '당뇨병 검사 결과 : ${snapshot2.data![index2].result}',
+                //                   style: const TextStyle(
+                //                     fontWeight: FontWeight.bold,
+                //                   ),
+                //                 ),
+                //               ],
+                //             ),
+                //           );
+                //         },
+                //       );
+                //     } else {
+                //       return const Center(child: CircularProgressIndicator());
+                //     }
+                  // },
+                // ),
+
+                // for Firebase
                 ListView(
                   shrinkWrap: true,
-                  children: snapshotList.map((e) => _buildItemWidget(e, context)).toList(),)
+                  children: snapshotList.map((e) => _buildItemWidget(e, context)).toList(),
+                ),
               ],
             );
           },
